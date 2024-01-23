2293a272a364f82cbd2a939781c86dc97414beb9
revathy <revathy@gmail.com>
2024-01-23 11:56:19 +0530

    client details form created

---
+++ curd/curd.js
@@ -0,0 +1,31 @@
+function client1() {
+    document.getElementById('client1-email').innerHTML = "E-mail:johndoe24@gmail.com";
+    document.getElementById('client1-phnno').innerHTML = "Phn.no:9236852458";
+}
+function client2() {
+    document.getElementById('client2-email').innerHTML = "E-mail:michael@gmail.com";
+    document.getElementById('client2-phnno').innerHTML = "Phn.no:9532746523";
+}
+function client3() {
+    document.getElementById('client3-email').innerHTML = "E-mail:emilyyam@gmail.com";
+    document.getElementById('client3-phnno').innerHTML = "Phn.no:958239874";
+}
+function client4() {
+    document.getElementById('client4-email').innerHTML = "E-mail:emma6@gmail.com";
+    document.getElementById('client4-phnno').innerHTML = "Phn.no:9852569458";
+}
+function client5() {
+    document.getElementById('client5-email').innerHTML = "E-mail:charles4@gmail.com";
+    document.getElementById('client5-phnno').innerHTML = "Phn.no:9580675439";
+}
+function client6() {
+    document.getElementById('client6-email').innerHTML = "E-mail:dimple54@gmail.com";
+    document.getElementById('client6-phnno').innerHTML = "Phn.no:9236852458";
+}
+function client7() {
+    document.getElementById('client7-email').innerHTML = "E-mail:yashana@gmail.com";
+    document.getElementById('client7-phnno').innerHTML = "Phn.no:8428870838";
+}
+
+
+
